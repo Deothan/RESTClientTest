b80b5ec4dd020000ec75b4bfb26070a3816598e7
Deothan <mads@deothan.dk>
2016-04-26 12:18:40 +0200

    rest client test

---
+++ app/src/main/java/dk/deothan/restclienttest/Greeting.java
@@ -0,0 +1,19 @@
+package dk.deothan.restclienttest;
+
+/**
+ * Created by Mads on 26-04-2016.
+ */
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
